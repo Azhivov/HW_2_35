a03195d9ab149e20e979ee11b6823505beedf91f
azhiv <a.zhivov@gmail.com>
2022-11-07 17:04:37 +0300

    first Commit

---
+++ src/main/java/com/example/hw_2_35/Hw235Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw235Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw235Application.class, args);
+    }
+
+}
